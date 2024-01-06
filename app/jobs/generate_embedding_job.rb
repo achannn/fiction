a69f2ddc5097e09d7a85515fd515e1d7916edb03
Andrew C <therealdoggard@gmail.com>
2024-01-06 03:49:04 +0100

    add retries to the job

--- app/jobs/generate_embedding_job.rb
+++ app/jobs/generate_embedding_job.rb
@@ -1,5 +1,6 @@
 class GenerateEmbeddingJob < ApplicationJob
   queue_as :default
+  retry_on Faraday::Error, wait: :polynomially_longer, attempts: 5
 
   def perform(record)
     record.generate_embedding
--- app/jobs/respond_to_chat_job.rb
+++ app/jobs/respond_to_chat_job.rb
@@ -1,5 +1,6 @@
 class RespondToChatJob < ApplicationJob
   queue_as :urgent
+  retry_on Faraday::Error, wait: :polynomially_longer, attempts: 5
 
   def perform(chat_message)
     chat_message.generate_reply
